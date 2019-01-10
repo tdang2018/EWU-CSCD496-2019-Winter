f329fa9684d0261324964bfa12058d70131a7adc
Tuan Dang <tdang6@outlook.com>
2019-01-10 15:31:20 -0800

    Wrote User class and UnitTest

---
+++ SecretSanta/src/SecretSanta.Domain/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/UserTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Tests.Models
+{
+   [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            User u = new User { FirstName = "Tuan", LastName = "Dang" };
+            Assert.AreEqual("Tuan", u.FirstName);
+        }
+
+    }
+}
