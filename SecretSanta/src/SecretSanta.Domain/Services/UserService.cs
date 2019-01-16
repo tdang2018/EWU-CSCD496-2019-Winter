4be8dd03b57e3c745f8bba1a22dda7e2fd63b50f
Tuan Dang <tdang6@outlook.com>
2019-01-15 22:23:12 -0800

    Create UserService and UserServiceTests

---
+++ SecretSanta/src/SecretSanta.Domain/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Services
+{
+    public class UserService
+    {
+        private ApplicationDbContext DbContext { get; set; }
+        public UserService(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public User AddUser(User user)
+        {
+            DbContext.Users.Add(user);           
+
+            DbContext.SaveChanges();
+
+            return user;
+        }
+
+        public User UpdateUser(User user)
+        {
+            DbContext.Users.Update(user);
+
+            DbContext.SaveChanges();
+
+            return user;
+        }
+
+        /*  public User Find(int id)
+          {
+              return DbContext.Users.Include(u => u.Posts).SingleOrDefault(u => u.Id == id);
+          }*/
+    }
+}
