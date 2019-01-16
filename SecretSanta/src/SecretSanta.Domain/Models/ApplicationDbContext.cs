9212a4a54dcdc2cdc2be0d6ad14f7e9a64d2df75
Tuan Dang <tdang6@outlook.com>
2019-01-15 21:25:21 -0800

    Create class ApplicationDbContext

---
+++ SecretSanta/src/SecretSanta.Domain/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Group> Posts { get; set; }
+        public  DbSet<User> Users{ get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {}
+     /*   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("DataSource=:memory:");
+        }*/
+    }
+}
