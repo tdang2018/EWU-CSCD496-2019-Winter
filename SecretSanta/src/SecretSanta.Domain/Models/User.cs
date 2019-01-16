cd47187af4e41a1180b4f16fee7e38622212b5dd
Tuan Dang <tdang6@outlook.com>
2019-01-15 22:20:26 -0800

    Create the properties for class User

--- SecretSanta/src/SecretSanta.Domain/Models/User.cs
+++ SecretSanta/src/SecretSanta.Domain/Models/User.cs
@@ -4,9 +4,15 @@ using System.Text;
 
 namespace SecretSanta.Domain.Models
 {
-    public class User
+    //hold the user's first and last name, list of gifts, groups the user belongs to
+    public class User : Entity
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public List<Gift> Gifts { get; set; }
+        //public List<Group> Groups { get; set; }
     }
+       
+    
 }
