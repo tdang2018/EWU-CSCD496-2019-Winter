640ee0edff5d9e0d792b5326506a274b68181583
Tuan Dang <tdang6@outlook.com>
2019-01-15 22:22:41 -0800

    Create GiftService and GiftServicesTests

---
+++ SecretSanta/src/SecretSanta.Domain/Services/GiftService.cs
@@ -0,0 +1,34 @@
+﻿using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Services
+{
+    public class GiftService
+    {
+        private ApplicationDbContext DbContext { get; set; }
+        public GiftService(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public Gift AddGift(Gift gift)
+        {
+            DbContext.Gifts.Add(gift);
+
+            DbContext.SaveChanges();
+
+            return gift;
+        }
+
+        public Gift UpdateGift(Gift gift)
+        {
+            DbContext.Gifts.Update(gift);
+
+            DbContext.SaveChanges();
+
+            return gift;
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Services/GiftServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Tests.Services
+{
+    [TestClass]
+    public class GiftServiceTests
+    {
+        Gift CreateGift()
+        {
+            var user = new User
+            {
+                FirstName = "Inigo",
+                LastName = "Montoya"
+            };
+
+            var gift = new Gift
+            {
+                Title = "My second gift",
+                OrderOfImportance = 2,
+                URL = "www.abc.com",
+                Description = "My second description",
+                User = user
+            };       
+            
+            return gift;
+        }
+
+        private SqliteConnection SqliteConnection { get; set; }
+        private DbContextOptions<ApplicationDbContext> Options { get; set; }
+
+        [TestInitialize]
+        public void OpenConnection()
+        {
+            SqliteConnection = new SqliteConnection("DataSource=:memory:");
+            SqliteConnection.Open();
+
+            Options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(SqliteConnection)
+
+                .EnableSensitiveDataLogging()
+                .Options;
+
+            using (var context = new ApplicationDbContext(Options))
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+        [TestCleanup]
+        public void CloseConnection()
+        {
+            SqliteConnection.Close();
+        }
+
+        [TestMethod]
+        public void AddGift()
+        {
+            using (var context = new ApplicationDbContext(Options))
+            {
+                GiftService service = new GiftService(context);
+                var myGift = CreateGift();
+
+                var persistedPost = service.AddGift(myGift);
+
+                Assert.AreNotEqual(0, persistedPost.Id);
+            }
+        }
+    }
+}
