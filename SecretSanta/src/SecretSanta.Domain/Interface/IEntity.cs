63770960288cb9bf98f924cd699a2813a5d70603
Tuan Dang <tdang6@outlook.com>
2019-01-15 21:24:25 -0800

    Create Interface IEntity

---
+++ SecretSanta/src/SecretSanta.Domain/Interface/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Interface
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+
+    }
+}
