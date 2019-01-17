d09c65eba38232fadd03287933336cced136b263
Tuan Dang <tdang6@outlook.com>
2019-01-17 12:42:50 -0800

    Adding 2 properties: Santa and Recipient type of User

--- SecretSanta/src/SecretSanta.Domain/Models/Pairing.cs
+++ SecretSanta/src/SecretSanta.Domain/Models/Pairing.cs
@@ -7,6 +7,7 @@ namespace SecretSanta.Domain.Models
     //Holds the User who is the recipient and the User who is the Santa for each group
     public class Pairing:Entity
     {
+        //public Group Group { get; set; }
         public User Santa { get; set; }
         public User Recipient { get; set; }
     }
