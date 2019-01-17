f0d7bf3c1a4543a2fa6c4807d129ff9a79912755
Tuan Dang <tdang6@outlook.com>
2019-01-17 12:42:14 -0800

    Add 2 more properties into class Message: ToUser, FromUser
    and Add group properties

--- SecretSanta/src/SecretSanta.Domain/Models/Group.cs
+++ SecretSanta/src/SecretSanta.Domain/Models/Group.cs
@@ -9,7 +9,7 @@ namespace SecretSanta.Domain.Models
     public class Group : Entity
     {
         public string Title { get; set; }
-        public List<User> Users { get; set; }
+       // public List<User> Users { get; set; }
     }
 
 }
--- SecretSanta/src/SecretSanta.Domain/Models/Message.cs
+++ SecretSanta/src/SecretSanta.Domain/Models/Message.cs
@@ -7,6 +7,8 @@ namespace SecretSanta.Domain.Models
     //Chat used so that Recipient and Santa can hold an anonymous discussion
     public class Message:Entity
     {
+        public User ToUser { get; set; }
+        public User FromUser { get; set; }
         public string MessageContent { get; set; }
     }
 }
