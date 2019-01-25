2cec4a202f3dbe79d6979b3985fe00f83ea90bd1
Kevin Bost <kevin@intellitect.com>
2019-01-24 21:05:47 -0800

    Adding example work from lecture on 1/24/19

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/GiftController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+
+namespace SecretSanta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftController : ControllerBase
+    {
+        private readonly IGiftService _GiftService;
+
+        public GiftController(IGiftService giftService)
+        {
+            _GiftService = giftService ?? throw new ArgumentNullException(nameof(giftService));
+        }
+
+        // GET api/Gift/5
+        [HttpGet("{userId}")]
+        public ActionResult<List<DTO.Gift>> GetGiftForUser(int userId)
+        {
+            if (userId <= 0)
+            {
+                return NotFound();
+            }
+            List<Gift> databaseUsers = _GiftService.GetGiftsForUser(userId);
+
+            return databaseUsers.Select(x => new DTO.Gift(x)).ToList();
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Api.Tests/GiftControllerTests.cs
@@ -0,0 +1,65 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecretSanta.Api.Tests
+{
+    [TestClass]
+    public class GiftControllerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GiftControl_RequiresGiftService()
+        {
+            new GiftController(null);
+        }
+
+        [TestMethod]
+        public void GetGiftForUser_ReturnsUsersFromService()
+        {
+            var gift = new Gift
+            {
+                Id = 3,
+                Title = "Gift Tile",
+                Description = "Gift Description",
+                Url = "http://www.gift.url",
+                OrderOfImportance = 1
+            };
+            var testService = new TestableGiftService
+            {
+                ToReturn =  new List<Gift>
+                {
+                    gift
+                }
+            };
+            var controller = new GiftController(testService);
+
+            ActionResult<List<DTO.Gift>> result = controller.GetGiftForUser(4);
+
+            Assert.AreEqual(4, testService.GetGiftsForUser_UserId);
+            DTO.Gift resultGift = result.Value.Single();
+            Assert.AreEqual(gift.Id, resultGift.Id);
+            Assert.AreEqual(gift.Title, resultGift.Title);
+            Assert.AreEqual(gift.Description, resultGift.Description);
+            Assert.AreEqual(gift.Url, resultGift.Url);
+            Assert.AreEqual(gift.OrderOfImportance, resultGift.OrderOfImportance);
+        }
+
+        [TestMethod]
+        public void GetGiftForUser_RequiresPositiveUserId()
+        {
+            var testService = new TestableGiftService();
+            var controller = new GiftController(testService);
+
+            ActionResult<List<DTO.Gift>> result = controller.GetGiftForUser(-1);
+            
+            Assert.IsTrue(result.Result is NotFoundResult);
+            //This check ensures that the service was not called
+            Assert.AreEqual(0, testService.GetGiftsForUser_UserId);
+        }
+    }
+}
