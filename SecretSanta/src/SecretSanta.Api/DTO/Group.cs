62e8c0d0ddef6723ab1a63cdbb4920cbf6725c5b
Tuan Dang <tdang6@outlook.com>
2019-01-31 12:04:27 -0800

    Added ToDomain and ToListDomain for class Group.cs

---
+++ SecretSanta/src/SecretSanta.Api/DTO/Group.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.DTO
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Group() { }
+
+        public Group(Domain.Models.Group group)
+        {
+            Id = group.Id;
+            Name = group.Name;
+        }
+
+        public static Domain.Models.Group ToDomain(DTO.Group group)
+        {
+            return new Domain.Models.Group { Id = group.Id, Name = group.Name };
+        }
+
+        public static List<Domain.Models.Group> ToListDomain(List<DTO.Group> group)
+        {
+            var listGroupDomain = new List<Domain.Models.Group>();
+            for ( int i = 0; i < group.Count; i++)
+                listGroupDomain.Add(new Domain.Models.Group { Id = group[i].Id, Name = group[i].Name });
+            return listGroupDomain;
+        }
+
+    }
+}
