00055a0f8d6eb6d2e7437cba975f7495682cb6bd
Tuan Dang <tdang6@outlook.com>
2019-01-31 12:06:10 -0800

    Create Interface for GroupSerivce

---
+++ SecretSanta/src/SecretSanta.Domain/Services/IGroupService.cs
@@ -0,0 +1,18 @@
+﻿using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Services
+{
+    public interface IGroupService
+    {
+        Group AddGroup(Group group);
+        Group UpdateGroup(Group group);
+        void DeleteGroup(Group group);
+        List<Group> FetchAll();
+        List<User> FetchAllUsersInGroup(int groupId);
+        User AddUserToGroup(int groupId, User user);
+        User RemoveUserFromGroup(int groupId, User user);
+    }
+}
