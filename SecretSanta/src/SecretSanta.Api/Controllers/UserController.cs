aa26ae1790d20cb43cc9beb0550b28d0651dc3b6
Tuan Dang <tdang6@outlook.com>
2019-01-31 12:03:19 -0800

    Added UserController.cs

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Domain.Services;
+
+namespace SecretSanta.Api.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _UserService;
+
+        public UserController(IUserService userService)
+        {
+            _UserService = userService ?? throw new ArgumentNullException(nameof(userService));
+        }    
+
+        //Post api/User
+        [HttpPost]
+        public ActionResult CreateUser(DTO.User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            _UserService.AddUser(DTO.User.ToDomain(user));
+            return Ok();
+        }
+
+        // PUT api/user
+        [HttpPut]
+        public ActionResult UpdateUser(DTO.User user)
+        {           
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            _UserService.UpdateUser(DTO.User.ToDomain(user));
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult RemoveUser(DTO.User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            _UserService.DeleteUser(DTO.User.ToDomain(user));
+
+            return Ok();
+        }
+    }
+}
