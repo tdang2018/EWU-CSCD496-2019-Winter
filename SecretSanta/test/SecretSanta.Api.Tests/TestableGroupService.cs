3c4c63d5f5f8ca746917ac8246c5b283961b7ab2
Tuan Dang <tdang6@outlook.com>
2019-01-31 12:08:18 -0800

    Added some testing methods for 2 classes TestableGroupSerivce and TestableUserService

---
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableGroupService.cs
@@ -0,0 +1,70 @@
+﻿using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests
+{
+    public class TestableGroupService : IGroupService
+    {
+
+        public Group AddGroup_Return { get; set; }
+        public Group AddGroup_Group { get; set; }
+
+        public Group AddGroup(Group group)
+        {
+            AddGroup_Group = group;
+            return AddGroup_Return;
+        }
+        
+        public Group DeleteGroup_Group { get; set; } 
+
+        public void DeleteGroup(Group group)
+        {
+            DeleteGroup_Group = group;
+            
+        }
+
+        public List<Group> FetchAll_Return { get; set; }
+        public List<Group> FetchAll()
+        {
+            return FetchAll_Return;
+        }
+
+        public Group UpdateGroup_Return { get; set; }
+        public Group UpdateGroup_Group { get; set; }
+        public Group UpdateGroup(Group group)
+        {
+            UpdateGroup_Group = group;
+            return UpdateGroup_Return;
+        }
+
+        public List<User> FetchAllUsersInGroup_Return { get; set; }
+        public int FetchAllUsersInGroup_groupId { get; set; }
+        public List<User> FetchAllUsersInGroup(int groupId)
+        {
+            FetchAllUsersInGroup_groupId = groupId;
+            return FetchAllUsersInGroup_Return;
+        }
+
+        public int AddUserToGroup_groupId { get; set; }
+        public User AddUserToGroup_User { get; set; }
+        public User AddUserToGroup_Return { get; set; }
+        public User AddUserToGroup(int groupId, User user)
+        {
+            AddUserToGroup_groupId = groupId;
+            AddUserToGroup_User = user;
+            return AddUserToGroup_Return;
+        }
+        public int RemoveUserFromGroup_groupId { get; set; }
+        public User RemoveUserFromGroup_User { get; set; }
+        public User RemoveUserFromGroup_Return { get; set; }
+        public User RemoveUserFromGroup(int groupId, User user)
+        {
+            RemoveUserFromGroup_groupId = groupId;
+            RemoveUserFromGroup_User = user;
+            return RemoveUserFromGroup_Return;
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableUserService.cs
@@ -0,0 +1,33 @@
+﻿using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests
+{
+    public class TestableUserService : IUserService
+    {
+        public User AddUser_Return { get; set; }
+        public User AddUser_User { get; set; }
+        public User AddUser(User user)
+        {
+            AddUser_User = user;
+            return AddUser_Return;
+        }
+
+        public User DeleteUser_User { get; set; }
+        public void DeleteUser(User user)
+        {
+            DeleteUser_User = user;
+        }
+
+        public User UpdateUser_User { get; set; }
+        public User UpdateUser_Return{get; set;}
+        public User UpdateUser(User user)
+        {
+            UpdateUser_User = user;
+            return UpdateUser_Return;
+        }
+    }
+}
