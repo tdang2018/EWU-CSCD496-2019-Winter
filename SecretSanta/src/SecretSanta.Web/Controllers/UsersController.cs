f911a40319a0f219917163de30f654b8e8597a15
Michael Stokesbary <mike@stokesbary.me>
2019-02-22 04:44:27 -0800

    Code from class on 2019.02.21

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Web.ApiModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecretSanta.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IHttpClientFactory ClientFactory { get; }
+        public UsersController(IHttpClientFactory clientFactory)
+        {
+            ClientFactory = clientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Users = await secretSantaClient.GetAllUsersAsync();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(UserInputViewModel viewModel)
+        {
+            IActionResult result = View();
+
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+                {
+                    try
+                    {
+                        var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                        await secretSantaClient.CreateUserAsync(viewModel);
+
+                        result = RedirectToAction(nameof(Index));
+                    }
+                    catch(SwaggerException se)
+                    {
+                        ViewBag.ErrorMessage = se.Message;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Web/webpack.config.js
@@ -0,0 +1,88 @@
+﻿// Lib
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const webpack = require('webpack');
+
+// Plugins
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production';
+
+    // Paths
+    const distPath = path.resolve(__dirname, './wwwroot');
+    const imgDistPath = path.resolve(distPath, './img');
+    const srcPath = '.';
+    const templatePath = path.resolve(__dirname, './Views/Shared');
+
+    return {
+        entry: './scripts/main.ts',
+        output: {
+            filename: 'js/[name].[hash].js',
+            path: distPath,
+            publicPath: '/',
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        { loader: MiniCssExtractPlugin.loader },
+                        { loader: 'css-loader' },
+                        { loader: 'postcss-loader', options: { ident: 'postcss', plugins: () => [autoprefixer()], }, },
+                        { loader: 'sass-loader' }
+                    ],
+                }
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(
+                [
+                    '**/*.js',
+                    '**/*.css'
+                ],
+                {
+                    root: distPath,
+                    watch: true,
+                },
+            ),
+
+            // Copy src images to wwwroot
+            new CopyWebpackPlugin(
+                [
+                    //{ from: 'favicon', to: distPath },
+                    //{ from: 'icons', to: iconDistPath },
+                    //{ from: 'images', to: imgDistPath },
+                ],
+                {
+                    context: srcPath,
+                    ignore: ['*.DS_Store'],
+                },
+            ),
+
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[hash].css',
+                path: distPath,
+                publicPath: '/',
+            }),
+
+            new HtmlWebpackPlugin({
+                filename: path.resolve(templatePath, './_Layout.cshtml'),
+                inject: false,
+                minify: false,
+                template: path.resolve(templatePath, './_Layout_Template.cshtml'),
+            })
+        ],
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+            modules: [
+                path.resolve(__dirname, './node_modules'),
+                srcPath,
+            ],
+        },
+    };
+}
