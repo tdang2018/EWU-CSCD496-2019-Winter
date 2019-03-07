b292afc2e4b5350477e1ed285dc05ec9fb526f42
Tuan Dang <tdang6@outlook.com>
2019-03-07 11:38:47 -0800

    Took picture and save image name CodeAnalyzer.png
    Do Code analysis

--- SecretSanta/src/SecretSanta.Web/Program.cs
+++ SecretSanta/src/SecretSanta.Web/Program.cs
@@ -19,6 +19,7 @@ namespace SecretSanta.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
